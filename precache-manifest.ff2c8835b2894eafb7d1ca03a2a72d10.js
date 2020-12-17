8206ccaaceb403e7fdfbbb1c8e76a6be9810a432
ievgend2 <ievgend2@gmail.com>
2020-12-16 21:11:28 -0500

    Mew initial deploy

---
+++ precache-manifest.ff2c8835b2894eafb7d1ca03a2a72d10.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ac27695650de40237ee693484f86f8",
+    "url": "/mayla_co/index.html"
+  },
+  {
+    "revision": "8d42131cb2866fd3691f",
+    "url": "/mayla_co/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "713c11284e130755ef79",
+    "url": "/mayla_co/static/css/main.5dccee53.chunk.css"
+  },
+  {
+    "revision": "8d42131cb2866fd3691f",
+    "url": "/mayla_co/static/js/2.f67e2ee0.chunk.js"
+  },
+  {
+    "revision": "b962909a7040b53b76bb931dd59e4242",
+    "url": "/mayla_co/static/js/2.f67e2ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "713c11284e130755ef79",
+    "url": "/mayla_co/static/js/main.647a4a88.chunk.js"
+  },
+  {
+    "revision": "c0c13c6ead1fa5aec63b",
+    "url": "/mayla_co/static/js/runtime-main.5fa9ace2.js"
+  },
+  {
+    "revision": "464dfa0905542aab60594e55e32b3b37",
+    "url": "/mayla_co/static/media/1.1.464dfa09.jpg"
+  },
+  {
+    "revision": "d6d0f92eec79cf8101ec84b1662baaad",
+    "url": "/mayla_co/static/media/1.2.d6d0f92e.jpg"
+  },
+  {
+    "revision": "180b5c7b71055aa7d4e86794a1159c22",
+    "url": "/mayla_co/static/media/1.3.180b5c7b.jpg"
+  },
+  {
+    "revision": "de9ab5388bea28a08a804265d2422370",
+    "url": "/mayla_co/static/media/couch1.de9ab538.jpg"
+  },
+  {
+    "revision": "d830b3b618a88ca630c43e6d381f5bc8",
+    "url": "/mayla_co/static/media/couch2.d830b3b6.jpg"
+  },
+  {
+    "revision": "bc2cb3fbc61b09711b3d8b54f46d0cb8",
+    "url": "/mayla_co/static/media/couch3.bc2cb3fb.jpg"
+  },
+  {
+    "revision": "3e43295a46217c1e4c5eb79fe2dc55d4",
+    "url": "/mayla_co/static/media/couch4.3e43295a.jpg"
+  }
+]);
